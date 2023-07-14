735d0507d1290123c5177bae38816f3f66d42db4
BrunoSouza <desouza88@hotmail.com>
2023-07-14 15:27:07 -0300

    feat: req02 - txt_importer function implemented

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+        return
+
+    if not os.path.exists(path_file):
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return
+
+    with open(path_file, "r") as f:
+        return f.read().split("\n")
