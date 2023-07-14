f1d045b1e80ae833c4ba9aa39122de4c4f9504a9
BrunoSouza <desouza88@hotmail.com>
2023-07-14 15:39:17 -0300

    feat: file_process.py implemented

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance.queue:
+        if item['nome_do_arquivo'] == path_file:
+            return
+
+    with open(path_file, 'r') as f:
+        lines = [line.rstrip('\n') for line in f]
+
+    data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(lines),
+        'linhas_do_arquivo': lines
+    }
+
+    instance.enqueue(data)
+
+    print(data)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.dequeue()
+        print(f"Arquivo {data['nome_do_arquivo']} removido com sucesso")
+    except IndexError:
+        print("Não há elementos")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        data = instance.search(position)
+        print(data)
+    except IndexError:
+        sys.stderr.write("Posição inválida")
